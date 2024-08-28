4d435390b0f1324976047c5fd86cf144790d5a44
wjx <jiaxuan.wjx@alibaba-inc.com>
2024-08-28 10:00:14 +0800

    update trantor cli version to [0.0.1], trantor commit[6634a18266196174026c792ad7771739025a3fd9]

---
+++ trantor2.rb
@@ -0,0 +1,35 @@
+class Trantor < Formula
+  desc "Terminus Trantor2 CLI"
+  homepage "https://www.terminus.io/"
+  url "https://terminus-trantor.oss-cn-hangzhou.aliyuncs.com/tools/cli/trantor2-cli.latest.tar.gz"
+  version "0.0.1"
+  sha256 "97a6f42c14398cfaccfdd694a7578bb67594eb0cb771096ef639f84e3ffd554c"
+
+#   depends_on "docker"
+
+  def buildExe()
+    <<~EOS
+      #!/bin/bash
+      if [ -z "$JAVA_HOME" ] ; then
+        JAVACMD=`which java`
+      else
+        JAVACMD="$JAVA_HOME/bin/java"
+      fi
+      export TRANTOR2_HOME="#{prefix}"
+      export TRANTOR2_CLI_VERSION="0.0.1"
+      exec "$JAVACMD" -jar "#{libexec}/trantor2-cli.jar" "$@"
+    EOS
+  end
+
+  def install
+    # Remove windows files
+    lib.install Dir["lib/*"]
+    libexec.install Dir["libexec/*"]
+    (bin/"trantor2").write buildExe()
+  end
+
+  test do
+    system "#{bin}/trantor2", "version"
+  end
+
+end
