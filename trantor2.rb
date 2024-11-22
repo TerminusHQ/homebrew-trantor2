dde32333da2e077e7e8cf002b943685530b5ee3b
wjx <jiaxuan.wjx@alibaba-inc.com>
2024-11-22 16:12:48 +0800

    update trantor cli version to [0.0.1], trantor commit[f1bc0d42463533efac8fefe011e51b9ea28c4795]

--- trantor2.rb
+++ trantor2.rb
@@ -3,14 +3,15 @@ class Trantor2 < Formula
   homepage "https://www.terminus.io/"
   url "https://terminus-trantor.oss-cn-hangzhou.aliyuncs.com/tools/cli2/trantor2-cli.latest.tar.gz"
   version "0.0.1"
-  sha256 "4778bf0636f3bd1296bdb47bde148ed9fd6355d62c647f6f3496d4d43d7d822f"
+  sha256 "b576d7d45bef182e4fa5fd543702cc1d1db98512e58e1b797e23f1a6d667b1cf"
 
-#   depends_on "docker"
-  skip_clean "java-runtime"
+  # 依赖 openjdk@17
+  depends_on "openjdk@17"
+
   def buildExe()
     <<~EOS
       #!/bin/bash
-      export JAVA_HOME="#{prefix}/java-runtime"
+      export JAVA_HOME="#{Formula["openjdk@17"].opt_prefix}"
       JAVACMD="$JAVA_HOME/bin/java"
       export TRANTOR2_HOME="#{prefix}"
       export TRANTOR2_CLI_VERSION="0.0.1"
@@ -19,8 +20,7 @@ class Trantor2 < Formula
   end
 
   def install
-    # Remove windows files
-    prefix.install %w[java-runtime]
+    # 安装主文件和库
     libexec.install Dir["libexec/*"]
     (bin/"trantor2").write buildExe()
   end
@@ -28,5 +28,4 @@ class Trantor2 < Formula
   test do
     system "#{bin}/trantor2", "version"
   end
-
 end
