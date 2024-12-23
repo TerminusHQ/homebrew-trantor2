0e1815bbdf3fb90bd9fdc3b56a8ceac00331ac24
wjx <jiaxuan.wjx@alibaba-inc.com>
2024-12-23 13:51:26 +0800

    update trantor cli version to [0.0.1], trantor commit[0bde89e7f31c69aa9ad046bb08447538ede3cacc]

--- trantor2.rb
+++ trantor2.rb
@@ -3,7 +3,7 @@ class Trantor2 < Formula
   homepage "https://www.terminus.io/"
   url "https://terminus-new-trantor.oss-cn-hangzhou.aliyuncs.com/tools/cli2/trantor2-cli.latest.tar.gz"
   version "0.0.1"
-  sha256 "3c3197e1e5448f9cbc36bc46ae629f5e821257b8d95da825fc4ae8bae6a56b50"
+  sha256 "7f7a2d08b5f4903ede0f6c88ae91e1fa0356e3cbbb9d09413d1a60afb0e7d1f6"
 
   # depends_on "docker"
   depends_on "openjdk@17"
