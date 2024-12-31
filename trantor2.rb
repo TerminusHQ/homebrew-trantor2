8a6053ed8342318fd8ddc06c194166f5cdaa8c07
Wujiaxuan007 <wujiaxuan1995@gmail.com>
2024-12-31 12:06:05 +0800

    update trantor cli version to [0.0.2], trantor commit[55b344b3fd68556f257f503d69eb692a2372e868]

--- trantor2.rb
+++ trantor2.rb
@@ -2,8 +2,8 @@ class Trantor2 < Formula
   desc "Terminus Trantor2 CLI"
   homepage "https://www.terminus.io/"
   url "https://terminus-new-trantor.oss-cn-hangzhou.aliyuncs.com/tools/cli2/trantor2-cli.latest.tar.gz"
-  version "0.0.1"
-  sha256 "a49ac71135a9e956b83f0bc1bbc3991aa9bb276e73946ba22a3733abf4ab0deb"
+  version "0.0.2"
+  sha256 "3196e0a9be2675df4278e86a5365caebb875dfba1c1ad9398eb7e486307610ee"
 
   # depends_on "docker"
   depends_on "openjdk@17"
@@ -15,7 +15,7 @@ class Trantor2 < Formula
       export JAVA_HOME="#{Formula["openjdk@17"].opt_prefix}"
       JAVACMD="$JAVA_HOME/bin/java"
       export TRANTOR2_HOME="#{prefix}"
-      export TRANTOR2_CLI_VERSION="0.0.1"
+      export TRANTOR2_CLI_VERSION="0.0.2"
       exec "$JAVACMD" -jar "#{libexec}/trantor2-cli.jar" "$@"
     EOS
   end
